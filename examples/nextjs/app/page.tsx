bc6a5e1875d29d765a0acc255c7cf88b41e05c59
Juancho Barroso <jbarroso@mymoid.com>
2024-01-03 09:01:34 +0000

    refactor: improve example

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -2,13 +2,28 @@ import { MymoidApi } from 'mymoid-api'
 
 export default async function Page() {
   const api = new MymoidApi()
-  const paymentOrderPage = await api.paymentOrders.getList({
-    page: 0,
-    limit: 5
-  }).catch((err) => {
-    console.log(err)
-    return null
-  })
-      
-  return <div>Payment Order: {paymentOrderPage?.content.length} </div>
+  const paymentOrderPage = await api.paymentOrders
+    .getList({
+      page: 0,
+      limit: 10,
+      status: ['PAID']
+    })
+    .catch((err) => {
+      console.log(err)
+      return null
+    })
+
+  return (
+    <div>
+      Payment Order:{' '}
+      {paymentOrderPage?.content.map((paymentOrder) => {
+        return (
+          <div key={paymentOrder.paymentOrderId}>
+            {paymentOrder.paymentOrderId} || {paymentOrder.amount} ||{' '}
+            {paymentOrder.status} || {paymentOrder.concept}
+          </div>
+        )
+      })}{' '}
+    </div>
+  )
 }
