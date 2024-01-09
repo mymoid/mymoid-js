880dfaf69bc68d5eeb3970b57894eb79ecc9f7a8
Juancho Barroso <jbarroso@mymoid.com>
2024-01-09 16:29:22 +0000

    refactor: add `makeRequest` to base endpoints class

--- packages/api/src/shared/utils.ts
+++ packages/api/src/shared/utils.ts
@@ -1,50 +1,9 @@
-import { MymoidApi } from '../api'
 import { ApiError } from '../errors/api-error'
 import { MymoidApiError } from './types'
 
-export async function makeRequest<TReturnType>(
-  api: MymoidApi,
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-  url: string,
-  body: any = undefined,
-  contentType: string | undefined = undefined
-): Promise<TReturnType> {
-  const apiKey = api.getApiKey()
-  const baseUrl = api.getBaseUrl()
-  const fullUrl = baseUrl + url
-  const organizationId = api.getOrganizationId()
-  const headers = {
-    'x-organization-id': organizationId,
-    'x-api-key': apiKey,
-    'Content-Type': contentType ?? 'application/json'
-  }
-
-  try {
-    const opts: RequestInit = {
-      method,
-      headers,
-      body: body
-        ? typeof body === 'string'
-          ? body
-          : JSON.stringify(body)
-        : undefined
-    }
-    const response = await fetch(fullUrl, opts)
-    if (!response.ok) {
-      const errorResponse = await response.json()
-      await handleError(errorResponse)
-    }
-    if (response.status === 204) {
-      return null as TReturnType
-    }
-    return await response.json()
-  } catch (error) {
-    // TODO: Add logic to this catch clause or eliminate it and rethrow the exception automatically. [sonarlint(typescript:S2737)]
-    throw error
-  }
-}
-
-async function handleError(error: MymoidApiError): Promise<MymoidApiError> {
+export async function handleError(
+  error: MymoidApiError
+): Promise<MymoidApiError> {
   throw new ApiError({
     code: error.code,
     message: error.message,
